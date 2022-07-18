9209ce629d4489062a2135a62181b2932bf9a33e
WangZhao <wz1847584786@163.com>
2022-07-18 20:29:34 +0800

    commit from Dell

---
+++ 141.环形链表.java
@@ -0,0 +1,40 @@
+/*
+ * @Date: 2022-07-15 17:43:28
+ * @LastEditTime: 2022-07-15 17:45:01
+ * @FilePath: \学习笔记d:\VSCode\LeetCode\141.环形链表.java
+ */
+/*
+ * @lc app=leetcode.cn id=141 lang=java
+ *
+ * [141] 环形链表
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ * int val;
+ * ListNode next;
+ * ListNode(int x) {
+ * val = x;
+ * next = null;
+ * }
+ * }
+ */
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        ListNode slow;
+        ListNode fast;
+    }
+
+    class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+}
+// @lc code=end
