3f7ffe360cb397acfe49772e49aa43845a893c1c
wangzhao <wz1847584786@163.com>
2022-08-03 22:20:02 +0800

    链表双指针

--- 160.相交链表.java
+++ 160.相交链表.java
@@ -1,6 +1,6 @@
 /*
  * @Date: 2022-07-28 20:59:54
- * @LastEditTime: 2022-07-30 09:52:05
+ * @LastEditTime: 2022-08-03 22:17:51
  * @FilePath: \Leetcode\160.相交链表.java
  */
 /*
@@ -22,43 +22,65 @@
  * }
  */
 class Solution {
+    // public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+    // ListNode curA = headA;
+    // ListNode curB = headB;
+    // int lengthA = 0;
+    // int lengthB = 0;
+    // while (curA != null) {
+    // lengthA++;
+    // curA = curA.next;
+    // }
+    // while (curB != null) {
+    // lengthB++;
+    // curB = curB.next;
+    // }
+    // curA = headA;
+    // curB = headB;
+    // //默认是A>B,如果不是则交换A，B
+    // if (lengthB > lengthA) {
+    // int temp = lengthA;
+    // lengthA = lengthB;
+    // lengthB = temp;
+    // ListNode tempNode = curA;
+    // curA = curB;
+    // curB = tempNode;
+    // }
+    // int gap = lengthA - lengthB;
+    // while (gap-- > 0) {
+    // curA = curA.next;
+    // }
+    // // 遍历A和B
+    // while (curA != null) {
+    // if (curA == curB) {
+    // return curA;
+    // }
+    // curA = curA.next;
+    // curB = curB.next;
+    // }
+    // return null;
+    // }
+    /**
+     * @description: 双指针法，逻辑上将两个链表连接在一起成环
+     * @param {ListNode} headA
+     * @param {ListNode} headB
+     * @return {*}
+     */
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
-        ListNode curA = headA;
-        ListNode curB = headB;
-        int lengthA = 0;
-        int lengthB = 0;
-        while (curA != null) {
-            lengthA++;
-            curA = curA.next;
-        }
-        while (curB != null) {
-            lengthB++;
-            curB = curB.next;
-        }
-        curA = headA;
-        curB = headB;
-        //默认是A>B,如果不是则交换A，B
-        if (lengthB > lengthA) {
-            int temp = lengthA;
-            lengthA = lengthB;
-            lengthB = temp;
-            ListNode tempNode = curA;
-            curA = curB;
-            curB = tempNode;
-        }
-        int gap = lengthA - lengthB;
-        while (gap-- > 0) {
-            curA = curA.next;
-        }
-        // 遍历A和B
-        while (curA != null) {
-            if (curA == curB) {
-                return curA;
+        ListNode p1 = headA, p2 = headB;
+        while (p1 != p2) {
+            if (p1 == null) {
+                p1 = headB;
+            } else {
+                p1 = p1.next;
             }
-            curA = curA.next;
-            curB = curB.next;
+            if (p2 == null) {
+                p2 = headA;
+            } else {
+                p2 = p2.next;
+            }
         }
-        return null;
+        return p1;
     }
 
 }
