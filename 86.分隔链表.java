070c928705ee65b287c1e3f72074c51127b42c8f
wangzhao <wz1847584786@163.com>
2022-08-03 18:32:15 +0800

    虚拟头结点的使用

---
+++ 86.分隔链表.java
@@ -0,0 +1,62 @@
+/*
+ * @Date: 2022-08-03 18:06:20
+ * @LastEditTime: 2022-08-03 18:20:05
+ * @FilePath: \LeetCode\86.分隔链表.java
+ */
+/*
+ * @lc app=leetcode.cn id=86 lang=java
+ *
+ * [86] 分隔链表
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ * int val;
+ * ListNode next;
+ * ListNode() {}
+ * ListNode(int val) { this.val = val; }
+ * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode partition(ListNode head, int x) {
+        ListNode dummy1 = new ListNode(-1), p1 = dummy1;
+        ListNode dummy2 = new ListNode(-1), p2 = dummy2;
+        ListNode p = head;
+        while (p != null) {
+            if (p.val < x) {
+                p1.next = p;
+                p1 = p1.next;
+            } else {
+                p2.next = p;
+                p2 = p2.next;
+            }
+            // 断开原链表中的每个节点的 next 指针
+            ListNode temp = p.next;
+            p.next = null;
+            p = temp;
+        }
+        p1.next = dummy2.next;
+        return dummy1.next;
+    }
+
+    // public class ListNode {
+    //     int val;
+    //     ListNode next;
+
+    //     ListNode() {
+    //     }
+
+    //     ListNode(int val) {
+    //         this.val = val;
+    //     }
+
+    //     ListNode(int val, ListNode next) {
+    //         this.val = val;
+    //         this.next = next;
+    //     }
+    // }
+}
+// @lc code=end
